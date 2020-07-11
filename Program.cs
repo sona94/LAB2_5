41d92c2737e1567014d15d0c87593653733e649d
sona94 <37668604+sona94@users.noreply.github.com>
2020-07-11 15:08:06 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LAB2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " +name);
+            // problems2 
+            int a = Convert.ToInt32 (Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine( a* b * c);
+
+            //problem3
+            Console.WriteLine("please write your age:");
+            string age = Console.ReadLine();
+            Console.WriteLine("You look younger than " +age);
+
+
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,3 @@
+﻿Problem 1: write your name from user
+Problem 2: Enter three numbers from user and do multiplication
+problem 3: Get the age from the user
